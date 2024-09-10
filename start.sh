8844444ca49435b69102aded85d80da693a68181
MichaelT-W23 <michael.totaro@icloud.com>
2024-09-10 18:27:49 -0400

    start.sh

--- start.sh
+++ start.sh
@@ -3,30 +3,30 @@ YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
-
-# Check if port is in use and kill the process
 kill_port_8080() {
   PID=$(lsof -t -i:8080)
 
   if [ -n "$PID" ]; then
-    echo "${YELLOW}Port 8080 is in use by PID $PID. Stopping the process...${NC}"
+    echo "\n${BLUE}Port 8080 is in use by PID $PID. Stopping the process...${NC}"
     kill -9 $PID
-    echo "${YELLOW}Process $PID has been stopped.${NC}"
+    echo "${BLUE}Process $PID has been stopped.${NC}"
   fi
-
 }
+
 
 stop_spring_boot() {
-  echo "\n\n${YELLOW}Spring Boot application finished successfully!${NC}"
-  kill "$SPRING_BOOT_PID"
+  echo "\n\n${YELLOW}Spring Boot application finished successfully! Stopping Spring Boot...${NC}"
+  kill "$SPRING_BOOT_PID" 2>/dev/null
+
+  kill_port_8080
 }
 
+# Handles control + c (SIGINT)/termination (SIGTERM)
 trap stop_spring_boot INT TERM
 
-# Check if port 8080 is in use and stop the process
 kill_port_8080
 
-# Run the Gradle bootRun task in the background
+# Runs the Gradle bootRun task in the background
 ./gradlew --quiet bootRun &
 SPRING_BOOT_PID=$!
 
@@ -34,4 +34,5 @@ sleep 0.1
 
 echo "\n* Running on ${BLUE}http://localhost:8080${NC}\n"
 
+# Wait for the Spring Boot process to finish
 wait "$SPRING_BOOT_PID"
